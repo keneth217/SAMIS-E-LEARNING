fb69305e1c733dd8e0fe2b9f2f0762f8c08c03fd
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-08 10:31:56 +0300

    update dark mode in admin components

--- src/views/admin/MyDashBoard.vue
+++ src/views/admin/MyDashBoard.vue
@@ -1,33 +1,32 @@
 <template>
-  <div class="my-dashboard">
-    <h1>My Dashboard</h1>
-    <p>Welcome to your personalized dashboard!</p>
-  </div>
 
-  <div class=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-    <div class="p-4 shadow-sm rounded-sm  bg-blue-400">
-      <h1>Total Courses</h1>
-      <p>100</p>
+  <div class="h-full">
+
+    <div class="my-dashboard">
+      <p class="mt-4 text-xl font-bold">Welcome back , Keneth!</p>
     </div>
 
-    <div class="p-4 shadow-sm rounded-sm  bg-blue-400">
-      <h1>Total Courses</h1>
-      <p>100</p>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+      <div class="p-4 shadow-sm rounded-sm bg-blue-400 dark:bg-gray-600">
+        <h1>Total Courses</h1>
+        <p>100</p>
+      </div>
+      <div class="p-4 shadow-sm rounded-sm bg-blue-400 dark:bg-gray-600">
+        <h1>Total Courses</h1>
+        <p>100</p>
+      </div>
+      <div class="p-4 shadow-sm rounded-sm bg-blue-400 dark:bg-gray-600">
+        <h1>Total Courses</h1>
+        <p>100</p>
+      </div>
     </div>
-    <div class="p-4 shadow-sm rounded-sm  bg-blue-400">
-      <h1>Total Courses</h1>
-      <p>100</p>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-20 cursor-pointer">
+      <router-link to="/admin/courses/add" class="p-4 shadow-sm rounded-sm bg-blue-400 dark:bg-gray-600">
+        <h1>Add New Course</h1>
+      </router-link>
     </div>
-
   </div>
-  <div class=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-20 cursor-pointer">
-    <router-link to="/dash/add-course" class="p-4 shadow-sm rounded-sm  bg-blue-400  ">
-      <h1>Add New Course</h1>
-
-    </router-link>
-
-  </div>
-
 </template>
 
 <script setup lang="ts">
