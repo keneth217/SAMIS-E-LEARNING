f8b91840edda9c2f6d9b129411a78256a4244891
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-08 17:23:08 +0300

    update dark mode in admin components

--- src/views/student/StudentDashboardData.vue
+++ src/views/student/StudentDashboardData.vue
@@ -3,23 +3,21 @@
   <div class="h-full">
 
     <div class="my-dashboard">
-      <h1>My Dashboard</h1>
-      <p>Welcome to your personalized dashboard!</p>
+      <p class="mt-4 text-xl font-bold">Welcome back , Student!</p>
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-      <div class="p-4 shadow-sm rounded-sm bg-blue-400">
+      <div class="p-4 shadow-sm rounded-sm bg-blue-400 dark:bg-gray-500">
         <h1>Enrolled Courses</h1>
         <p>100</p>
       </div>
-
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-20 cursor-pointer">
-      <router-link to="/dash/add-course" class="p-4 shadow-sm rounded-sm bg-blue-400">
-        <h1>Add New Course</h1>
-      </router-link>
-    </div>
+<!--    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-20 cursor-pointer">-->
+<!--      <router-link to="/dash/add-course" class="p-4 shadow-sm rounded-sm bg-blue-400">-->
+<!--        <h1>Enrolled </h1>-->
+<!--      </router-link>-->
+<!--    </div>-->
   </div>
 
 </template>
