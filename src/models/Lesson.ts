4ecc0a01f41b49b0dbcc83cf1985a5807de0bb55
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-21 20:56:19 +0300

    Add authentication functionalities and some components

---
+++ src/models/Lesson.ts
@@ -0,0 +1,46 @@
+
+export interface CreateLessonRequest {
+    file: File;
+    title: string;
+    description: string;
+}
+
+export interface DeleteLessonRequest {
+    lessonID: number;
+}
+
+export interface VideoUploadResponse {
+    success: boolean;
+    message: string;
+    filename: string;
+    uploadDir: string;
+    lesson: LessonDAO;
+}
+
+export interface LessonDAO {
+    lessonID: number;
+    title: string;
+    description: string;
+    filename: string;
+    destination: string;
+    createdAt?: string;
+    updatedAt?: string;
+}
+
+export interface ListLessonsResponse {
+    success: boolean;
+    lessons: LessonDAO[];
+}
+
+export interface DeleteLessonResponse {
+    success: boolean;
+    message: string;
+    lesson: LessonDAO | null;
+}
+
+
+export interface ApiError {
+    success: boolean;
+    message: string;
+    errors?: Record<string, string[]>;
+}
