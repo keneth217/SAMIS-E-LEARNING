097444f0d146520fed27d95221cbe5b7d589356c
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-21 20:56:35 +0300

    Add authentication functionalities and some components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,28 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
-// Admin views
+
 import AdminDashBoard from '@/views/admin/AdminDashBoard.vue'
 import MyDashBoard from '@/views/admin/MyDashBoard.vue'
 import AddCourse from '@/views/admin/Add-course.vue'
 import CourseContent from '@/views/admin/CourseContent.vue'
 
-// Student views
+
 import StudentDashboard from '@/views/student/StudentDashboard.vue'
 import StudentDashboardData from '@/views/student/StudentDashboardData.vue'
 import EnrolledCourses from '@/views/student/EnrolledCourses.vue'
 
-// Shared views
+
 import HomeView from '../views/HomeView.vue'
 import ViewCourseDetails from "@/views/student/ViewCourseDetails.vue";
 import Teachers from "@/views/admin/Teachers.vue";
+import LessonListView from "@/views/admin/LessonListView.vue";
+import LessonDetailView from "@/views/admin/LessonDetailView.vue";
+import LoginView from "@/views/admin/LoginView.vue";
+import RegisterView from "@/views/admin/RegisterView.vue";
+import ChangePassword from "@/views/admin/ChangePassword.vue";
+import UsersLists from "@/views/admin/UsersLists.vue";
+import * as path from "node:path";
+import TeacherDashboard from "@/views/teacher/TeacherDashboard.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +39,33 @@ const router = createRouter({
                 title: 'Home'
             }
         },
+
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+            meta: {
+                title: 'Authenticate'
+            }
+        },
+
+        {
+            path: '/reset-password',
+            name: 'reset-password',
+            component: ChangePassword,
+            meta: {
+                title: 'Change Password'
+            }
+        },
+
+        {
+            path: '/register',
+            name: 'Register',
+            component: RegisterView,
+            meta: {
+                title: 'Register'
+            }
+        },
         {
             path: '/about',
             name: 'about',
@@ -43,11 +78,7 @@ const router = createRouter({
             path: '/admin',
             name: 'admin',
             component: AdminDashBoard,
-            // meta: {
-            //     requiresAuth: true,
-            //     role: 'admin',
-            //     title: 'Admin Dashboard'
-            // },
+
             children: [
                 {
                     path: '',
@@ -80,8 +111,39 @@ const router = createRouter({
                     meta: {
                         title: 'Add New Course'
                     }
-                }
+                },
+                {
+                    path: 'users-lists',
+                    name: 'users-list',
+                    component: UsersLists
+                },
+                {
+                    path: 'lessons',
+                    name: 'lessons-list',
+                    component: LessonListView
+                },
+                {
+                    path: 'lessons/:id',
+                    name: 'lesson-detail',
+                    component: LessonDetailView,
+                    props: true
+                },
             ]
+        },
+
+
+        {
+            path:'/teachers-dash'
+            ,name:'teachers-dashboard',
+            component: TeacherDashboard,
+            children: [
+                {
+                    path:'/my-lessons'
+                    ,name:'teachers',
+                    component: TeacherDashboard,
+                }
+
+            ],
         },
         {
             path: '/student',
